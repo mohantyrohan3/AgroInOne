fa2f1d7984082520ca5c6e2f308ca61df7aa72eb
Rohan Mohanty <mohantyrohan3@gmail.com>
2023-03-21 09:14:30 +0530

    First Push of Project

---
+++ frontend/src/App.js
@@ -0,0 +1,14 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello World</h1>
+      <h2>Helllo All !!</h2>
+      <p>My Project Name is AgroInOne</p>
+    </div>
+  );
+}
+
+export default App;
