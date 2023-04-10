a75dcacac633c37874bfa32ed23bbc024f7432cd
Rohan Mohanty <mohantyrohan3@gmail.com>
2023-04-10 22:06:54 +0530

    Added more cards

--- frontend/src/components/SelectDropdown.js
+++ frontend/src/components/SelectDropdown.js
@@ -1,15 +1,15 @@
 import * as React from 'react';
-import Box from '@mui/material/Box';
-import InputLabel from '@mui/material/InputLabel';
-import MenuItem from '@mui/material/MenuItem';
-import FormControl from '@mui/material/FormControl';
-import Select from '@mui/material/Select';
+// import Box from '@mui/material/Box';
+// import InputLabel from '@mui/material/InputLabel';
+// import MenuItem from '@mui/material/MenuItem';
+// import FormControl from '@mui/material/FormControl';
+// import Select from '@mui/material/Select';
 import Container from 'react-bootstrap/Container';
-import styles from './Selectstyle.css';
+import  './Selectstyle.css';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
-import TotalSchemeCard from "./TotalSchemeCard.jsx";
-import data from './api/govt_schemes.json';
+import TotalSchemeCard from "./TotalSchemeCard.js";
+import data from '../api/govt_schemes.json';
 
 
 
@@ -18,7 +18,7 @@ export default function SelectDropdown() {
   const [age, setAge] = React.useState('');
   let temp2=[]
   for(let i=0;i<data.length;i++){
-      if(data[i]['State']=="All India"){
+      if(data[i]['State']==="All India"){
         temp2.push(data[i]);
       }
   }
@@ -29,7 +29,7 @@ export default function SelectDropdown() {
     let temp = [];
     let temp1 = event.target.value;
     for(let i=0;i<data.length;i++){
-      if(data[i]['State']==temp1){
+      if(data[i]['State']===temp1){
         temp.push(data[i]);
       }
     }
@@ -37,9 +37,9 @@ export default function SelectDropdown() {
   };
 
 
-  const returnChange = () =>{
-    console.log(age);
-  }
+  // const returnChange = () =>{
+  //   console.log(age);
+  // }
 
   return (
     <>
@@ -56,10 +56,14 @@ export default function SelectDropdown() {
                       <select onChange={handleChange} className="selectpicker form-control border-0  px-4 py-4 rounded shadow  select-heading">
                         <option value="" selected disabled hidden>SELECT STATE</option>
                         <option value="All India">All India</option>
-                        <option value="Haryana">Haryana</option>
+                        <option value="Delhi">Delhi</option>
                         <option value="Odisha">Odisha</option>
                         <option value="Punjab">Punjab</option>
                         <option value="UP">UP</option>
+                        <option value="Jharkhand">Jharkhand</option>
+                        <option value="Himachal Pradesh">Himachal Pradesh</option>
+                        <option value="Haryana">Haryana</option>
+                        <option value="Bihar">Bihar</option>
                       </select>
                     </div>
                   </div>
